792c4677e8492b6d4e5b5b98dbe651227be355eb
wesleyleite <wesleyhenrique@gmail.com>
2013-10-10 00:07:03 -0300

    eliminando primeiro ciclo da formula e removido limitacao de 99 pacotes

--- timer.sh
+++ timer.sh
@@ -6,7 +6,6 @@ export LC_NUMERIC="POSIX"
 
 ALFA=0.125 # 1/8
 BETA=0.25  # 1/4
-flag=0
 MS=""
 TPKTS=0
 DHOST=""
@@ -34,8 +33,10 @@ disparar() {
     local destino=$2
     echo
     [ -z "${destino}" ] && destino="uw.edu"
-    echo ">> Disparando contra [ ${destino} ]"
-    MS=$( ping -c ${pkts} ${destino} 2>> log.txt | grep -Ewo 'time=([0-9]){1,}\ ms' | sed -r 's/time=|ms//g' )
+    echo ">> Disparando contra [ ${destino} ( ${pkts} ) ]"
+    MS=$( ping -c ${pkts} ${destino} 2>> log.txt | 
+        grep -Ewo 'time=([0-9]){1,}\ ms' | 
+        sed -r 's/time=|ms//g' )
 }
 
 gerar() {
@@ -46,22 +47,27 @@ gerar() {
     local VAR
     local SVAR
     local RTO
+    local flag=0
 
     echo "     RTT                 SRTT                SVAR                RTO"
     for RTT in ${MS}
     do
-        SRTT=0
-        VAR=0
-        SVAR=0
-        RTO=0
-        #otimizar, remover |, e.g. let SRTT=(1-$ALFA)*$SRTT + $ALFA*$RTT
-        #SRTT=$(((1-ALFA)*SRTT+ALFA*RTT))
-        SRTT=$(echo "(1-${ALFA})*$SRTT+${ALFA}*${RTT}" | bc)
-        VAR=$(echo "${RTT} - ${SRTT}" | bc )
-        #otmizar, maneira mais inteligente de remover sinal
-        VAR=$(echo $VAR | sed 's/^-//')
-        SVAR=$(echo "(1-${BETA})*${SVAR}+${BETA}*${VAR}" | bc )
-        RTO=$(echo "${SRTT}+4*${SVAR}" | bc)
+        [ ${flag} -eq 1 ] && { 
+            #otimizar, remover |, e.g. let SRTT=(1-$ALFA)*$SRTT + $ALFA*$RTT
+            #SRTT=$(((1-ALFA)*SRTT+ALFA*RTT))
+            SRTT=$(echo "(1-${ALFA})*$SRTT+${ALFA}*${RTT}" | bc)
+            VAR=$(echo "${RTT} - ${SRTT}" | bc )
+            #otmizar, maneira mais inteligente de remover sinal
+            VAR=$(echo $VAR | sed 's/^-//')
+            SVAR=$(echo "(1-${BETA})*${SVAR}+${BETA}*${VAR}" | bc )
+            RTO=$(echo "${SRTT}+4*${SVAR}" | bc)
+        } || {
+            RTT0=$RTT
+            SRTT=$RTT0
+            let SVAR=$RTT0/2
+            RTO=0
+            flag=1
+        }
         printf "%04d %07d %20f %20f %20f\n" $((i++)) ${RTT} ${SRTT} ${SVAR} ${RTO}
     done
 }
@@ -72,8 +78,10 @@ gerar() {
 # validar se quantidade de pacotes enviadas é numerico
 # pegar somente dois primeiros limitando a ate 99
 TPKTS=$1
-TPKTS=${TPKTS:0:2}
-TPKTS=$( echo ${TPKTS} | grep -Ewo '^([0-9]){1,2}$' )
+#TPKTS=${TPKTS:0:2 }
+TPKTS=$( echo ${TPKTS} | 
+        grep -Ewo '^[0-9]+$' )
+
 [ $# -le 0 ] || [ -z ${TPKTS} ] && ajuda 
 
 DHOST=$2
@@ -83,9 +91,7 @@ disparar ${TPKTS} $2
 gerar
 
  
-#verificar quantidade de parametros
 #considerar perdas
-#permitir a passagem do host de forma opcional
 #implementar pacotes udp, tcp e icmp
 #verificar jitter
 # See more at: http://www.devin.com.br/shell_script/#sthash.tCFaYGI7.dpuf
